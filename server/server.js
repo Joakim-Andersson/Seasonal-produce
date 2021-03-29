28c90cec695e3ab6b600901c19031d8a5cceca83
Joakim-Andersson <jjoakimm@hotmail.com>
2021-03-29 11:06:16 +0200

    add env info

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ require('dotenv').config();
 const ID = process.env.API_ID;
 const KEY = process.env.API_KEY;
 
-const baseURL = `https://api.edamam.com/search?&app_id=${ID}&app_key=${KEY}&to=10&health=vegan`
+// const baseURL = `https://api.edamam.com/search?&app_id=${ID}&app_key=${KEY}&to=10&health=vegan`
+const baseURL = `https://api.edamam.com/search?&app_id=${API_ID}&app_key=${API_KEY}&to=10&health=vegan`
 
 // endpoints
 app.listen(port, () => {
