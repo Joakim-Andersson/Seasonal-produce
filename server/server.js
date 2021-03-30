80f50d92b6619e44074b1c2a835f392046c51e98
Joakim-Andersson <jjoakimm@hotmail.com>
2021-03-30 09:25:49 +0200

    Update CORS

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-var cors = require('cors')
+// var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 8000;
 var fetch = require('node-fetch')
 const mockdata = require("./mochdata.json")
 
-app.use(cors())
+// app.use(cors())
 
 app.use(function (req, res, next) {
 
