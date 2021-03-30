c7480e523bd250d73ccf519dcb0671db2b36438a
Joakim-Andersson <jjoakimm@hotmail.com>
2021-03-30 09:18:10 +0200

    Update CORS

--- server/server.js
+++ server/server.js
@@ -7,6 +7,16 @@ const mockdata = require("./mochdata.json")
 
 app.use(cors())
 
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 global.fetch = fetch;
 
 // Api .env
